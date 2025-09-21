ffdb80f9991eb003c1804e62627c18bc58638b29
nabeelhussain13 <nh944314@gmail.com>
2025-09-21 17:46:07 +0500

    Added Express lec-6

---
+++ ExpressJS Tutorial/6 - Dynamic UI using EJS/routes/hostRouter.js
@@ -0,0 +1,23 @@
+// Core Module
+const path = require("path");
+
+// External Module
+const express = require("express");
+const hostRouter = express.Router();
+
+// Local Module
+const rootDir = require("../utils/pathUtil");
+
+hostRouter.get("/add-home", (req, res, next) => {
+  res.render("addHome", { pageTitle: "Add Home" });
+});
+
+const registeredHomes = [];
+
+hostRouter.post("/add-home", (req, res, next) => {
+  registeredHomes.push({ houseName: req.body.houseName });
+  res.render("homeAdded", { pageTitle: "Home Added" });
+});
+
+exports.hostRouter = hostRouter;
+exports.registeredHomes = registeredHomes;
